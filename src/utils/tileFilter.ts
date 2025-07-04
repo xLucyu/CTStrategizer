2abbe0e77e8d5bd7cd2ceded1cb66c2271135775
Lucy <xlucyyu@gmail.com>
2025-07-04 10:14:01 +0200

    Update tileFilter.ts

---
+++ src/utils/tileFilter.ts
@@ -0,0 +1,15 @@
+const gameTypes: Record<number, string> = {
+    2: "Race",
+    4: "Boss",
+    8: "Least Cash",
+    9: "Least Tiers"
+}
+
+const bossType: Record<number, string> = {
+    0: "Bloonarius",
+    1: "Vortex",
+    2: "Lych",
+    3: "Dreadbloon",
+    4: "Phayze",
+    5: "Blastapopulous"
+}
